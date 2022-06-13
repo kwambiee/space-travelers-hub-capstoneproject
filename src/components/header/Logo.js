5ab6bd4a8f9b52f4e8c7a6ebc9d5e41acd9835fa
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-13 12:08:01 +0100

    add header, headerlinks and  logo

---
+++ src/components/header/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../images/plamnt.png";
+
+export default function Logo() {
+  return (
+    <div>
+      <img src={logo} width={70} height={70} alt="planet-logo" />
+      <h1>Space Traveler&apos; s Hub</h1>
+    </div>
+  );
+}
