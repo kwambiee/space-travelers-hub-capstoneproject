4970fd712139f4beacbe027c149bc7642a31c74f
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-13 19:07:58 +0300

    Add reducer for mission action

--- src/redux/mission/mission.js
+++ src/redux/mission/mission.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const MISSION_ADDED = "react-app/redux/MISSION_ADDED";
+const MISSION_FAILURE = "react-app/redux/MISSION_FAILURE";
 
 const AddMission = (mission) => ({
   type: MISSION_ADDED,
@@ -10,9 +11,19 @@ const AddMission = (mission) => ({
 const fetchMission = () => async (dispatch) => {
   axios
     .get("https://api.spacexdata.com/v3/missions")
-    .then((response) => console.log(response.data))
-    .then((err) => err.message);
+    .then((response) => dispatch(AddMission(response.data)))
+    .then((err) => dispatch({ type: MISSION_FAILURE, err }));
 };
-fetchMission();
+
+const InitialState = [];
+
+export const MissionReducer = (state = InitialState, action) => {
+  switch (action.type) {
+    case MISSION_ADDED:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
 
 export default fetchMission;
