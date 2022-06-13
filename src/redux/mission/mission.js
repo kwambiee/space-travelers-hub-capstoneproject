6d89330076a3e7525e30c973973e9c143cc0222c
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-13 18:32:19 +0300

    Add action to add mission

--- src/redux/mission/mission.js
+++ src/redux/mission/mission.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+const MISSION_ADDED = "react-app/redux/MISSION_ADDED";
+
+const AddMission = (mission) => ({
+  type: MISSION_ADDED,
+  payload: mission,
+});
+
 const fetchMission = () => async (dispatch) => {
   axios
     .get("https://api.spacexdata.com/v3/missions")
