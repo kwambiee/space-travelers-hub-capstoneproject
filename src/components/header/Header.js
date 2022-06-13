72d47ee9822b02eb9353847c427ea9375e1ca53f
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-13 12:18:19 +0100

    add pages

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import HeaderLinks from './HeaderLinks';
+import Logo from './Logo';
+import './header.css';
+
 
 export default function Header() {
     return (
-        <div>
-            
+        <div className='header'>
+            <Logo />
+            <HeaderLinks /> 
         </div>
     );
 }
