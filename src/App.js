179ac04ad89488beea88c3866f9872afdfd03ad4
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-13 16:06:39 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/header/Header";
-import HomePage from "./components/pages/Home";
-import Missions from "./components/pages/Mission";
-import Profile from "./components/pages/Profile";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import HomePage from './components/pages/Home';
+import Missions from './components/pages/Mission';
+import Profile from './components/pages/Profile';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/missions' element={<Missions />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
