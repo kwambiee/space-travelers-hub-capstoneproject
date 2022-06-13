682e4b065dbc6603855821c8cc0ff82275e03277
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-13 19:04:46 +0300

    Cinfigure the store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { combineReducers, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { MissionReducer } from "./mission/mission";
+
+const rootReducer = combineReducers({ mission: MissionReducer });
+const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
+
+export default store;
