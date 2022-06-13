71d305e5d8479146840bcbeaa48555cf08f8ed63
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-13 21:35:36 +0100

    fix linter

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,10 @@
 import { combineReducers, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { MissionReducer } from './mission/mission';
 import rocketReducer from './rockets/rocket';
 import fetchRocketThunk from './rockets/rocketThunk';
-
 
 const rootReducer = combineReducers({ mission: MissionReducer, rocketReducer });
 const store = configureStore({ reducer: rootReducer }, applyMiddleware(logger, thunk));
