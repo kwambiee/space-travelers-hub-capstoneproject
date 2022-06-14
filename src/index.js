ac3ee83e8c5251faf9e9ba5b554825ccb546585b
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-14 10:56:30 +0100

    fix linter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
