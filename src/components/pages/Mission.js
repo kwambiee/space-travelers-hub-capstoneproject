bbc42b86cc321b998ac06023dad3db2c4b9b60ef
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-14 19:38:09 +0300

    Fix linters

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,14 +1,13 @@
-import React, { useEffect } from "react";
-import "./styles.css";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from 'react';
+import './styles.css';
+import { useSelector, useDispatch } from 'react-redux';
 import fetchMission, {
   joinMission,
   leaveMission,
-} from "../../redux/mission/mission";
+} from '../../redux/mission/mission';
 
 export default function Missions() {
   const missionsApi = useSelector((state) => state.MissionReducer.missions);
-  console.log(missionsApi);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -41,20 +40,20 @@ export default function Missions() {
               <td>{mission.description}</td>
               <td>
                 <div>
-                  <h6>{mission.reserved ? "ACTIVE MEMBER" : "NOT A MEMBER"}</h6>
+                  <h6>{mission.reserved ? 'ACTIVE MEMBER' : 'NOT A MEMBER'}</h6>
                 </div>
               </td>
               <td>
                 {mission.reserved ? (
                   <button
-                    type='button'
+                    type="button"
                     onClick={() => handleLeave(mission.mission_id)}
                   >
                     Leave Mission
                   </button>
                 ) : (
                   <button
-                    type='button'
+                    type="button"
                     onClick={() => handleJoin(mission.mission_id)}
                   >
                     Join Mission
--- src/redux/mission/mission.js
+++ src/redux/mission/mission.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-const MISSION_ADDED = "react-app/redux/MISSION_ADDED";
-const MISSION_FAILURE = "react-app/redux/MISSION_FAILURE";
-const JOIN_MISSION = "react-app/redux/JOIN_MISSION";
-const LEAVE_MISSION = "react-app/redux/LEAVE_MISSION";
+const MISSION_ADDED = 'react-app/redux/MISSION_ADDED';
+const MISSION_FAILURE = 'react-app/redux/MISSION_FAILURE';
+const JOIN_MISSION = 'react-app/redux/JOIN_MISSION';
+const LEAVE_MISSION = 'react-app/redux/LEAVE_MISSION';
 
 const AddMission = (mission) => ({
   type: MISSION_ADDED,
@@ -21,31 +21,32 @@ export const leaveMission = (id) => ({
 });
 
 const fetchMission = () => async (dispatch) => {
-  await axios.get("https://api.spacexdata.com/v3/missions").then(
+  await axios.get('https://api.spacexdata.com/v3/missions').then(
     (response) => dispatch(AddMission(response.data)),
-    (err) => dispatch({ type: MISSION_FAILURE, err })
+    (err) => dispatch({ type: MISSION_FAILURE, err }),
   );
 };
 
 const InitialState = { missions: [] };
 
 export const MissionReducer = (state = InitialState, action) => {
+  const Index = state.missions.findIndex(
+    (obj) => obj.mission_id === action.payload,
+  );
+  const newArr = [...state.missions];
+  const Arr = state.missions.findIndex(
+    (obj) => obj.mission_id === action.payload,
+  );
+  const leaveArr = [...state.missions];
+
   switch (action.type) {
     case MISSION_ADDED:
       return { ...state, missions: action.payload };
     case JOIN_MISSION:
-      const index = state.missions.findIndex(
-        (obj) => obj.mission_id == action.payload
-      );
-      const newArr = [...state.missions];
-      newArr[index].reserved = true;
+      newArr[Index].reserved = true;
       return { ...state, missions: [...newArr] };
     case LEAVE_MISSION:
-      const i = state.missions.findIndex(
-        (obj) => obj.mission_id == action.payload
-      );
-      const leaveArr = [...state.missions];
-      leaveArr[i].reserved = false;
+      leaveArr[Arr].reserved = false;
       return { ...state, missions: [...leaveArr] };
     default:
       return state;
