b39cd36e1527c750f9ae8f4bafe4e676c32863d3
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-14 10:53:14 +0100

    fix linter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
+      <Provider store= {store} >
         <App />
       </Provider>
     </Router>
