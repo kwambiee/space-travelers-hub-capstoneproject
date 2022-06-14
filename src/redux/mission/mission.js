3ec4e565acfab3e3cd2076d5411f67b1945bf4f4
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-14 14:01:28 +0300

    Add action and reducer for leave mission

--- src/redux/mission/mission.js
+++ src/redux/mission/mission.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const MISSION_ADDED = "react-app/redux/MISSION_ADDED";
 const MISSION_FAILURE = "react-app/redux/MISSION_FAILURE";
 const JOIN_MISSION = "react-app/redux/JOIN_MISSION";
+const LEAVE_MISSION = "react-app/redux/LEAVE_MISSION";
 
 const AddMission = (mission) => ({
   type: MISSION_ADDED,
@@ -11,6 +12,11 @@ const AddMission = (mission) => ({
 
 const joinMission = (mission_id) => ({
   type: JOIN_MISSION,
+  payload: mission_id,
+});
+
+const leaveMission = (mission_id) => ({
+  type: LEAVE_MISSION,
   payload: mission_id,
 });
 
@@ -28,11 +34,15 @@ export const MissionReducer = (state = InitialState, action) => {
     case MISSION_ADDED:
       return { ...state, missions: action.payload };
     case JOIN_MISSION:
-      const newState = state.filter(
+      const joinState = state.filter(
         (Amission) => Amission.id !== action.payload
       );
-      return { ...newState, reserved: true };
-
+      return { ...joinState, reserved: true };
+    case LEAVE_MISSION:
+      const leaveState = state.filter(
+        (Amission) => Amission.id !== action.payload
+      );
+      return { ...leaveState, reserved: false };
     default:
       return state;
   }
