2a50f1d9c4f5937b14f86bb5d6aa3a8e7605d707
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-15 23:00:18 +0100

    add test file for rockets

---
+++ src/tests/Rockets.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import store from '../redux/configureStore';
+import HomePage from '../components/pages/Home';
+import '@testing-library/jest-dom/extend-expect';
+
+const RocketsProvider = () => (
+  <Provider store={store}>
+    <HomePage />
+  </Provider>
+);
+
+describe('Check Homepage', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(<RocketsProvider />).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('No Rocket Available', () => {
+  it('renders correctly', () => {
+    render(<RocketsProvider />);
+    expect(screen.getByText('No rockets available')).toBeInTheDocument();
+  });
+});
+
+describe('Rockets Component', () => {
+  it('renders rockets', async () => {
+    render(<RocketsProvider />);
+    const allRockets = await screen.findAllByText('Reserve Rocket');
+    expect(allRockets).toHaveLength(4);
+  });
+
+  test('Reserve a rocket and cancel reservation', async () => {
+    render(<RocketsProvider />);
+    const reserveButton = await screen.findAllByText('Reserve Rocket');
+    const badges = await screen.findAllByText('Reserved');
+    expect(reserveButton[0].textContent).toBe('Reserve Rocket');
+    fireEvent.click(reserveButton[0]);
+    expect(badges[0].textContent).toBe('Reserved');
+    expect(reserveButton[0].textContent).toBe('Cancel Reservation');
+  });
+});
