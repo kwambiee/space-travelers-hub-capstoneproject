c466ba5bd3b834c35a63b207f7f2d2cf5e5662c4
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-16 12:31:47 +0300

    Fix linters

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import MyRockets from "./reservedRocket";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import MyRockets from './reservedRocket';
 
 const Profile = () => {
   const [missionJoined, setMissionJoined] = useState([]);
@@ -15,24 +15,24 @@ const Profile = () => {
   }, []);
 
   const rocketState = useSelector(
-    (state) => state.rocketReducer.rockets
+    (state) => state.rocketReducer.rockets,
   ).filter((rocket) => rocket.reserved === true);
 
   return (
     <div>
-      <div className='profile-container'>
-        <span className='profile-missions'>
+      <div className="profile-container">
+        <span className="profile-missions">
           <h2>My Missions</h2>
           <ul>
             {missionJoined.map((mission) => (
-              <li className='profile-item' key={mission.mission_id}>
+              <li className="profile-item" key={mission.mission_id}>
                 {mission.mission_name}
               </li>
             ))}
           </ul>
         </span>
 
-        <span className='profile-rockets'>
+        <span className="profile-rockets">
           <h2>My Rockets</h2>
           <ul>
             <MyRockets data={rocketState} />
--- src/components/pages/Rocket.js
+++ src/components/pages/Rocket.js
@@ -1,12 +1,14 @@
-import React from "react";
-import "./rockets.css";
-import PropTypes from "prop-types";
-import { useDispatch } from "react-redux";
-import { reserveRocket } from "../../redux/rockets/rocket";
+import React from 'react';
+import './rockets.css';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../../redux/rockets/rocket';
 
 const RocketCard = (props) => {
   const dispatch = useDispatch();
-  const { rocketName, description, flickrImages, reserved, id } = props;
+  const {
+    rocketName, description, flickrImages, reserved, id,
+  } = props;
 
   const handleRocketReserve = (rocketId) => {
     dispatch(reserveRocket(rocketId));
@@ -14,37 +16,40 @@ const RocketCard = (props) => {
 
   const reserveButtons = !reserved ? (
     <button
-      type='button'
-      className='rocket-button'
+      type="button"
+      className="rocket-button"
       onClick={() => handleRocketReserve(id)}
     >
       Reserve Rocket
     </button>
   ) : (
     <button
-      type='button'
+      type="button"
       onClick={() => handleRocketReserve(id)}
-      className='rocket-cancel'
+      className="rocket-cancel"
     >
       Cancel Reservation
     </button>
   );
 
   return (
-    <div className='rocket-card'>
+    <div className="rocket-card">
       <img
         src={flickrImages}
         width={200}
         height={200}
         alt={rocketName}
-        style={{ borderRadius: "3px" }}
+        style={{ borderRadius: '3px' }}
       />
-      <div className='rocket-card-content'>
-        <h1> {rocketName}</h1>
+      <div className="rocket-card-content">
+        <h1>
+          {' '}
+          {rocketName}
+        </h1>
         <p>
           <span
-            className='rocket-reserve'
-            style={{ display: `${!reserved ? "none" : "inline"}` }}
+            className="rocket-reserve"
+            style={{ display: `${!reserved ? 'none' : 'inline'}` }}
           >
             Reserved
           </span>
