920e4d0c1753952d2b14c7376d90e8c474383ab9
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-16 14:58:59 +0100

    fix linters

--- src/test/RocketReducer.test.js
+++ src/test/RocketReducer.test.js
@@ -1,20 +1,20 @@
-import rocketReducer from "../redux/rockets/rocket";
+import rocketReducer from '../redux/rockets/rocket';
 
-describe("Test reducers, actions and components", () => {
-  test("should return the initial state", () => {
+describe('Test reducers, actions and components', () => {
+  test('should return the initial state', () => {
     expect(rocketReducer(undefined, {})).toEqual({ rockets: [] });
   });
 
-  test("should handle FETCH_ROCKETS", () => {
+  test('should handle FETCH_ROCKETS', () => {
     const state = { rockets: [] };
-    const FETCH_ROCKETS = "react-app/redux/FETCH";
+    const FETCH_ROCKETS = 'react-app/redux/FETCH';
     const fetchRockets = () => ({
       type: FETCH_ROCKETS,
     });
     expect(rocketReducer(state, fetchRockets)).toEqual({ rockets: [] });
   });
 
-  test("should handle TOGGLE_RESERVE", () => {
+  test('should handle TOGGLE_RESERVE', () => {
     const state = { rockets: [] };
     const TOGGLE_RESERVE = 'react-app/redux/RESERVE';
     const reserveRocket = () => ({
