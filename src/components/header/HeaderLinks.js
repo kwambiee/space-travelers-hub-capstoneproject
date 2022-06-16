a7635302e38a9a63a77a90826369cea7587304de
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-16 15:15:15 +0100

    fix header link to match UI design

--- src/components/header/HeaderLinks.js
+++ src/components/header/HeaderLinks.js
@@ -4,13 +4,13 @@ import { NavLink } from 'react-router-dom';
 export default function HeaderLinks() {
   return (
     <div className="links">
-      <NavLink to="/" className="link">
+      <NavLink to="/" className={(link) => (link.isActive ? 'active-link' : 'link')}>
         Rockets
       </NavLink>
-      <NavLink to="/missions" className="link">
+      <NavLink to="/missions" className={(link) => (link.isActive ? 'active-link' : 'link')}>
         Missions
       </NavLink>
-      <NavLink to="/profile" className="link">
+      <NavLink to="/profile" className={(link) => (link.isActive ? 'active-link' : 'link')}>
         Profile
       </NavLink>
     </div>
